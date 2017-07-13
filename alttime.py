24883efbaf9dc614fe45750248d4e76d60a85c66
Sean Trinh <strinh@stevens.edu>
2017-07-13 12:03:58 -0400

    Create alttime.py

---
+++ alttime.py
@@ -0,0 +1,17 @@
+import ntplib
+from time import ctime
+import time
+import sys
+
+try:
+        while True:
+                c=ntplib.NTPClient()
+                response=c.request('us.pool.ntp.org',version=3)
+                print(ctime(response.tx_time))
+                time.sleep(10)
+
+except KeyboardInterrupt:
+        print("\n A keyboard interrupt has been noticed")
+
+except:
+        print("An error or exception has occurred")

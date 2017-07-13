1e99720990b0566b8f6796c67773cc2f0738ea05
Sean Trinh <strinh@stevens.edu>
2017-07-13 08:58:08 -0400

    Update batterytest.py

--- batterytest.py
+++ batterytest.py
@@ -13,14 +13,17 @@ db = MySQLdb.connect(
 curs=db.cursor()
 #counter=0
 try:
-        while True:
-                #counter += 1
-                time.sleep(10)
-                updateCounter = ("UPDATE batterytest SET c = c + 1")
-                curs.execute(updateCounter)
-                db.commit()
-                print("Counter updated")
-
+        curs.execute('SELECT * FROM batterytest')
+        data = curs.fetchall()
+        if str(data[0]) == '(0L,)':
+                while True:
+                        time.sleep(10)
+                        updateCounter = ("UPDATE batterytest SET c = c + 1")
+                        curs.execute(updateCounter)
+                        db.commit()
+                        print("Counter updated")
+        else:
+                print("Test complete")
 except KeyboardInterrupt:
         print "\nA keyboard interrupt has been noticed"
 
